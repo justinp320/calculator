a2c7da81a8e910b90ba060d63ca99544abde821c
justinp320 <33816208+justinp320@users.noreply.github.com>
2021-09-11 23:19:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,170 @@
+let firstOperand = '';
+let secondOperand = '';
+let currentOperator = null;
+let isExponential = false;
+let expValue = 0;
+
+const displayNumbers = document.getElementById('answer');
+const displayMath = document.getElementById('math');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const clearButton = document.querySelector('[data-clear]');
+const backButton = document.querySelector('[data-backspace]');
+const signButton = document.querySelector('[data-sign]');
+const equalsButton = document.querySelector('[data-equals]');
+
+clearButton.addEventListener('click', clear);
+backButton.addEventListener('click', deleteNumber);
+signButton.addEventListener('click', changeSign);
+equalsButton.addEventListener('click', equals);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        appendNumber(button.textContent);
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        setOperator(button.textContent);
+    });
+});
+
+function appendNumber(number){
+    // ensure only one decimal point
+    if (number=='.' && displayNumbers.textContent.includes('.')){
+        displayNumbers.textContent = displayNumbers.textContent;
+    }
+    else if (displayNumbers.textContent == '0'){
+        displayNumbers.textContent = number;
+    }
+    else if (displayNumbers.textContent == '-0'){
+        displayNumbers.textContent = `-${number}`;
+    }
+    else if ((displayNumbers.textContent.length >= 10)){
+        isExponential = true;
+        expValue = Number(displayNumbers.textContent += number) 
+        displayNumbers.textContent = expValue.toExponential(2);
+    }
+    else if (isExponential){
+        expValue = Number(expValue.toString() + number);
+        displayNumbers.textContent = expValue.toExponential(2);
+    }
+    else{
+        displayNumbers.textContent += number;
+    }
+}
+
+function setOperator(operator){
+    isExponential = false;
+    expValue = 0;
+    if (currentOperator != null){
+        currentOperator = operator;
+        displayMath.textContent = `${firstOperand} ${currentOperator} `;
+    }
+    else{
+
+    firstOperand = displayNumbers.textContent;
+    displayNumbers.textContent = '';
+    currentOperator = operator;
+    displayMath.textContent = `${firstOperand} ${currentOperator} `;
+    }
+}
+
+function changeSign(){
+    if (displayNumbers.textContent == 0){
+        displayNumbers.textContent = '-0';
+    }
+    else{
+    displayNumbers.textContent = Number(displayNumbers.textContent)*(-1);
+    }
+}
+
+function clear(){
+    displayNumbers.textContent = '0';
+    displayMath.textContent = '';
+    isExponential = false;
+    expValue = 0;
+}
+
+function deleteNumber(){
+    let num = displayNumbers.textContent.toString();
+    if ((num.length == 1) || (num.length==2 && num.includes('-'))){
+        clear();
+    }
+    else{
+        displayNumbers.textContent = num.slice(0, -1);
+    }
+}
+
+function equals(){
+    if (currentOperator=='÷' && (Number(displayNumbers.textContent)==0 || firstOperand == 0)){
+        alert("You cannot divide by zero");
+        return;
+    }
+    if (currentOperator!=null){
+        secondOperand = displayNumbers.textContent;
+        displayMath.textContent+= ` ${secondOperand} =`;
+        ans = Math.round ((operate(firstOperand, secondOperand, currentOperator)) * 1000) / 1000;
+        if (ans.toString().length >= 10){
+            displayNumbers.textContent = ans.toExponential(2);
+            currentOperator = null;
+        }
+        else{
+            displayNumbers.textContent = ans;
+            currentOperator = null;
+        }
+    }
+}
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(a, b, operator){
+    a = Number(a);
+    b = Number(b);
+    switch(operator){
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '×':
+            return multiply(a, b);
+        case '÷':
+            return divide(a, b);
+    }
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key=='Backspace'){
+        deleteNumber();
+    }
+    else if (e.key=='Enter' || e.key=='='){
+        equals();
+    }
+    else if (e.key=='+' || e.key=='-'){
+        setOperator(e.key);
+    }
+    else if (e.key=='*'){
+        setOperator('×');
+    }
+    else if (e.key=='/'){
+        setOperator('÷');
+    }
+    else if ((e.key <= 9 && e.key >= 0) || e.key=='.'){
+        appendNumber(e.key);
+    }
+});
